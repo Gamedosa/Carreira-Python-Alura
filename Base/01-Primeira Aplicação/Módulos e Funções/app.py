c3a10e1d701e9412d9ee292c492cdbab5af64f0b
Gamedosa <gabriel20melo@gmail.com>
2026-01-07 22:59:07 -0300

    feat: modulo função completo.

--- Base/01-Primeira Aplicação/Módulos e Funções/app.py
+++ Base/01-Primeira Aplicação/Módulos e Funções/app.py
@@ -1,10 +1,41 @@
-print ('Sabor Express\n')
+import os
+def exibir_nome_do_progama(): 
+    print('''
+                                                                                                   
+  ▄▄▄▄         █                           ▄▄▄▄▄▄                                          
+ █▀   ▀  ▄▄▄   █▄▄▄    ▄▄▄    ▄ ▄▄         █      ▄   ▄  ▄▄▄▄    ▄ ▄▄   ▄▄▄    ▄▄▄    ▄▄▄  
+ ▀█▄▄▄  ▀   █  █▀ ▀█  █▀ ▀█   █▀  ▀        █▄▄▄▄▄  █▄█   █▀ ▀█   █▀  ▀ █▀  █  █   ▀  █   ▀ 
+     ▀█ ▄▀▀▀█  █   █  █   █   █            █       ▄█▄   █   █   █     █▀▀▀▀   ▀▀▀▄   ▀▀▀▄ 
+ ▀▄▄▄█▀ ▀▄▄▀█  ██▄█▀  ▀█▄█▀   █            █▄▄▄▄▄ ▄▀ ▀▄  ██▄█▀   █     ▀█▄▄▀  ▀▄▄▄▀  ▀▄▄▄▀ 
+                                                         █                                 
+                                                         ▀                                 
+          ''')
 
-print('1.Cadastrar Restaurante\n')
-print('2.Listar Restaurante\n')
-print('3.Ativar Restaurante\n')
-print('4.Sair\n')
+def exibir_opcoes():
+    print('1.Cadastrar Restaurante\n')
+    print('2.Listar Restaurante\n')
+    print('3.Ativar Restaurante\n')
+    print('4.Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando app')
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_progama()
+    exibir_opcoes()
+    escolher_opcao()
+    
+if __name__ == '__main__':
+    main()
